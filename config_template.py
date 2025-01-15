07147d7fed8875c34c4469343420822001b78037
lingduyu <ry.micool@gmail.com>
2025-01-14 22:28:23 -0600

    minor bug fix

--- config_template.py
+++ config_template.py
@@ -1,9 +1,9 @@
 class Config:
     DATABASE = "stories.db"
-    api_key = ""
-    base_url = "https://api.deepseek.com"
+    API_KEY = ""
+    BASE_RUL = "https://api.deepseek.com"
     TTS_SERVICES = {
     'gtts': 'Google Text-to-Speech',
     'f5tts': 'F5 Text-to-Speech'
     }
-    f5tts_url = 'http://localhost:8000/tts'
+    F5TTS_URL = 'http://localhost:8000/tts'
